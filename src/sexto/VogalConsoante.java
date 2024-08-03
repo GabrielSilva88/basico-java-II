7166fa942e53ad6a0e6878e2bce90fe6cd2745df
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-08-03 10:37:53 -0300

    Exercicios quarto ao sexto exercicio.

---
+++ src/sexto/VogalConsoante.java
@@ -0,0 +1,22 @@
+/*Escreva um programa que leia uma letra do alfabeto e diga se ela é uma vogal ou uma consoante. 
+	Lembre-se que, na comparação de Strings, usa-se o método .equals() ou o .equalsIgnoreCase().
+ */
+package sexto;
+
+import java.util.Scanner;
+
+public class VogalConsoante {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Verificar se a letra e Vogal ou Consoante");
+
+		System.out.println("Digite a letra, agora: ");
+		String letra = scan.next();
+		
+		
+
+	}
+
+}
